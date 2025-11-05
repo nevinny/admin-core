48d6717ac49d19c428b5a7b9f80a943c5e267999
zyablik <nevinny@gmail.com>
2025-11-05 20:47:45 +0300

    fix

--- src/AdminCore.php
+++ src/AdminCore.php
@@ -5,6 +5,17 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AdminCore extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/Resources/config')
+        );
+        $loader->load('services.yaml');
+    }
+
     public function getPath(): string
     {
         return \dirname(__DIR__);
